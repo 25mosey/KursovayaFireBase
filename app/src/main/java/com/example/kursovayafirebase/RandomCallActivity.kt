75029173c74a318f6c38fa7fa7cf399379f72004
Nikita <nikm100804@gmail.com>
2024-02-05 12:03:03 +0300

    сломано(починить по возможности)

--- app/src/main/java/com/example/kursovayafirebase/RandomCallActivity.kt
+++ app/src/main/java/com/example/kursovayafirebase/RandomCallActivity.kt
@@ -1,20 +1,32 @@
 package com.example.kursovayafirebase
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.RecyclerView
 
 class RandomCallActivity : AppCompatActivity() {
+
+    private lateinit var textViewForId: TextView
+    private lateinit var textViewForName: TextView
+    private lateinit var textViewForAddress: TextView
+    private lateinit var textViewForProblem: TextView
+    private lateinit var btnBack: Button
+    private lateinit var request: Request
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_random)
 
-        val btnBack: Button = findViewById(R.id.btnBack)
-
+        btnBack = findViewById(R.id.btnBack)
         btnBack.setOnClickListener(View.OnClickListener {
             finish()
         })
     }
+
+
 }
+
